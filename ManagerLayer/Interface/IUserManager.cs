06089b51ad56a02a303f7cbed2d5e52e604890eb
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-24 14:56:47 +0530

    [Priyanshu] Add: Added ManagerLayer to Project

---
+++ ManagerLayer/Interface/IUserManager.cs
@@ -0,0 +1,14 @@
+﻿using CommonLayer.RequestModel;
+using RepositoryLayer.Enitity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Interface
+{
+    public interface IUserManager
+    {
+        public UserEntity UserRegistration(RegisterModel model);
+    }
+}
+    
---
+++ ManagerLayer/Services/UserManager.cs
@@ -0,0 +1,24 @@
+﻿using CommonLayer.RequestModel;
+using ManagerLayer.Interface;
+using RepositoryLayer.Enitity;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Services
+{
+    public class UserManager : IUserManager
+    {
+        private readonly IUserInterface repository;
+        public UserManager(IUserInterface repository)
+        {
+            this.repository = repository;
+        }
+
+        public UserEntity UserRegistration(RegisterModel model)
+        {
+            return repository.UserRegistration(model);
+        }
+    }
+}
