587eeae25951646774e76bc8e94fbf844110e6c2
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-24 14:55:30 +0530

    [Priyanshu] Add: Added Common Layer

---
+++ CommonLayer/RequestModel/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CommonLayer.RequestModel
+{
+    public class RegisterModel
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
