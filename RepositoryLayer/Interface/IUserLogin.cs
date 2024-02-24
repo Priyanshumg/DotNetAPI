baad1095bfbebf64cf6bd2d623d591ffc1682a2f
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-24 16:02:10 +0530

    [Priyanshu] Add: Added Interface Element to the RepositoryLayer for LoginService

---
+++ RepositoryLayer/Interface/IUserLogin.cs
@@ -0,0 +1,13 @@
+﻿using CommonLayer.RequestModel;
+using RepositoryLayer.Enitity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    public interface IUserLogin
+    {
+        public UserEntity UserLogin(LoginModel model);
+    }
+}
