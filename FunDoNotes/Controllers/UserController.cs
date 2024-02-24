5a875ae5a7aa661e0e4860252d86d67cb8af41fd
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-24 14:56:16 +0530

    [Priyanshu] Add: Added presentation Layer as FunDoNotes

---
+++ FunDoNotes/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ManagerLayer.Interface;
+using CommonLayer.RequestModel;
+using CommonLayer.ResponseModel;
+using RepositoryLayer.Enitity;
+
+namespace FunDoNotes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserManager userManager;
+        public UserController(IUserManager userManager)
+        {
+            this.userManager = userManager;
+        }
+        [HttpPost]
+        [Route("Reg")]
+        public ActionResult Register (RegisterModel model)
+        {
+            var response = userManager.UserRegistration(model);
+            if (response != null)
+            {
+                return Ok(new ResponseModel<UserEntity> { Success= true, Message="Register Successfull", Data=response});
+            }
+            else
+            {
+                return BadRequest(new ResponseModel<UserEntity> { Success = false, Message = "Register Failed", Data = response });
+            }
+        }
+    }
+}
