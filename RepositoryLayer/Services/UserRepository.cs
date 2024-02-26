a58c3456bd22da23854e7fd3b8be4095d32dc704
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-26 15:23:53 +0530

    [Priyanshu] Add: Added Generate Token method to UserRepository in RepositoryLayer

--- RepositoryLayer/Services/UserRepository.cs
+++ RepositoryLayer/Services/UserRepository.cs
@@ -5,13 +5,18 @@ using RepositoryLayer.Interface;
 using System;
 using Microsoft.EntityFrameworkCore.Internal;
 using System.Linq;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
 
 namespace RepositoryLayer.Services
 {
     public class UserRepository : IUserInterface
     {
         private readonly UserContext context;
-
+        private readonly IConfiguration _config;
         public UserRepository(UserContext context)
         {
             this.context = context;
@@ -42,7 +47,7 @@ namespace RepositoryLayer.Services
             {
                 if (userEntity.UserEmail != null)
                 {
-                    if (userEntity.UserEmail == model.User_Email)
+                    if (userEntity.UserEmail == model.UserEmail)
                     {
                         if (userEntity.UserPassword == model.UserPassword)
                         {
@@ -67,6 +72,23 @@ namespace RepositoryLayer.Services
             {
                 throw;
             }
+        }
+        private string GenerateToken(string Email, string UserId)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim("Email",Email),
+                new Claim("UserId", UserId)
+            };
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddHours(15),
+                signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
         }
     }
 }
