3519b628a9c1616bf8ba04e8cf707b570e4b9d80
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-27 13:55:10 +0530

    [Priyanshu] fix merge conflict

--- RepositoryLayer/obj/Debug/netcoreapp3.1/RepositoryLayer.AssemblyInfo.cs
+++ RepositoryLayer/obj/Debug/netcoreapp3.1/RepositoryLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf894e1c2802531457d91d7cca1bd6f0a74af35f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20f4e5c83e39dbe10871872b1b2575b1a1f852a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
