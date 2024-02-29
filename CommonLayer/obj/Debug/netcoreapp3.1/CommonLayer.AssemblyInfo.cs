b6fc4acb43357ccd35572807c62a4ea9c9e18558
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-02-29 14:46:42 +0530

    [Priyanshu] fix: Fixed Assembly build refference

--- CommonLayer/obj/Debug/netcoreapp3.1/CommonLayer.AssemblyInfo.cs
+++ CommonLayer/obj/Debug/netcoreapp3.1/CommonLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommonLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f74dbdbd784182e496dc9dc988c63ca037ae6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd69f1757b679ce7c6a274ede7e69ca983f0d052")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommonLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommonLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
