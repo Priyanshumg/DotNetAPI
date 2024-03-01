aa8bb6293886b5970b7fd86d533b1d6416bd82a1
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-01 15:09:49 +0530

    [Priyanshu] Add: Added the ForgetPassword method and elements to the ManagerLayer

--- ManagerLayer/Services/UserManager.cs
+++ ManagerLayer/Services/UserManager.cs
@@ -26,6 +26,11 @@ namespace ManagerLayer.Services
             return repository.UserLogin(model);
         }
 
+        public ForgetPasswordModel ForgetPassword(string UserEmail)
+        {
+            return repository.ForgetPassword(UserEmail);
+        }
+
         public bool ResetPassword(string Email, ResetPasswordModel model)
         {
             return repository.ResetPassword(Email, model);
