bdf8a1b177d334c9cabd2c042b0ed5fc430f0c05
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-01 16:00:20 +0530

    [Priyanshu] Add: Added a folder name as LoginPageModel for organisation Purpose

---
+++ CommonLayer/RequestModel/LoginPageModel/ResetPasswordModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.RequestModel
+{
+    public class ResetPasswordModel
+    {
+        //public string UserEmail;
+        public string newPassword { get; set; }
+    }
+}
