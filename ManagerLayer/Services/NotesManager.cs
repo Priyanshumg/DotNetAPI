7d7a4ba20365c86f452a14bb01fbde902ae8063e
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-02 11:15:57 +0530

    [Priyanshu] Add: Added CreateNote To ManagerLayer/Service

--- ManagerLayer/Services/NotesManager.cs
+++ ManagerLayer/Services/NotesManager.cs
@@ -1,11 +1,26 @@
-﻿using ManagerLayer.Interface;
+﻿using CommonLayer.RequestModel.NotesModel;
+using ManagerLayer.Interface;
+using Repository.Services;
+using RepositoryLayer.Enitity;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ManagerLayer.Services
 {
-    public class NotesManager : INotesManager
+    public class NoteManager : INotesManager
     {
+        private readonly INotesInterface repository;
+        public NoteManager(INotesInterface repository)
+        {
+            this.repository = repository;
+        }
+        public NotesEntity CreateNote(CreateNotes model, int Id)
+        {
+            return repository.CreateNote(model, Id);
+        }
     }
+
 }
