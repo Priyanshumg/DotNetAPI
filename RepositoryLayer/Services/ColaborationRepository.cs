766cd9a407ecbf76ee79cefe6bbecb30df03af3c
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-04 16:34:49 +0530

    [Priyanshu] Add: Added repository for ColaborationAPI

---
+++ RepositoryLayer/Services/ColaborationRepository.cs
@@ -0,0 +1,18 @@
+﻿using RepositoryLayer.Context;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Services
+{
+    public class ColaborationRepository : IColaborationInterface
+    {
+        private readonly UserContext context;
+
+        public ColaborationRepository(UserContext context)
+        {
+            this.context = context;
+        }
+    }
+}
