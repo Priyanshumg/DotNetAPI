633f78d0d1fe412846d27364e5646708b6446a8b
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-04 07:36:06 +0530

    [Priyanshu] Add: Added Interface element for the LabelManager

---
+++ ManagerLayer/Services/LabelManager.cs
@@ -0,0 +1,29 @@
+﻿using CommonLayer.RequestModel.LabelModel;
+using ManagerLayer.Interface;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Services
+{
+    public class LabelManagers : ILabelManager
+    {
+        private readonly ILabelInterface LabelRepository;
+
+        public LabelManagers(ILabelInterface repository)
+        {
+            this.LabelRepository = repository;
+        }
+        public LabelEntity CreateLabel(LabelModel model, int noteId)
+        {
+            return LabelRepository.CreateLabel(model, noteId);
+        }
+
+        public int GetNoteIdByName(string noteName)
+        {
+            return LabelRepository.GetNoteIdByName(noteName);
+        }
+    }
+}
