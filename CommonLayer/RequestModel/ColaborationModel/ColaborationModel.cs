0e467737269286b886e6dc0f389c2d55c6d5228f
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-04 16:40:53 +0530

    [Priyanshu] Add: Added ColaborationModel for parametric use

---
+++ CommonLayer/RequestModel/ColaborationModel/ColaborationModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.RequestModel.ColaborationModel
+{
+    public class ColaborationModel
+    {
+
+    }
+}
