6e56b1d8d00dbc1c428d44993a50562f4c5e2a1b
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-04 08:43:53 +0530

    [Priyanshu] Add: Added AssignLabel to the ManagerLayer

--- ManagerLayer/Services/LabelManager.cs
+++ ManagerLayer/Services/LabelManager.cs
@@ -30,5 +30,10 @@ namespace ManagerLayer.Services
         {
             return LabelRepository.DisplayAllLabel();
         }
+
+        public LabelEntity AssignLabel(int labelId, int noteId)
+        {
+            return LabelRepository.AssignLabel(labelId, noteId);
+        }
     }
 }
