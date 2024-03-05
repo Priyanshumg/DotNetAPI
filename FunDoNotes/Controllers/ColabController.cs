964344490bf789986f16f573c87a06e33b97f91d
priyanshu <ginkalpriyanshu@gmail.com>
2024-03-05 12:46:06 +0530

    [Priyanshu] Add: Added ControllerElement for the ColabController

---
+++ FunDoNotes/Controllers/ColabController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FunDoNotes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColabController : ControllerBase
+    {
+    }
+}
